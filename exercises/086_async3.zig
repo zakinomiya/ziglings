9aae0a057cf9bf28fc85685f7524241c2900f786
Atsushi Miyazaki <ki929.oa95cp@gmail.com>
2022-03-01 11:08:55 +0900

    completed all exercises!!

--- exercises/086_async3.zig
+++ exercises/086_async3.zig
@@ -13,7 +13,10 @@ pub fn main() void {
     const n = 5;
     var foo_frame = async foo(n);
 
-    ???
+    var i: i8 = 0;
+    while (i < n) : (i += 1) {
+        resume foo_frame;
+    }
 
     print("\n", .{});
 }
